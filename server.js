98cb2d6f3048b079573e918d6f059a96a7fd604a
fran_abad <fam1096@hotmail.com>
2023-04-26 16:02:06 +0200

    Add register

--- server.js
+++ server.js
@@ -2,12 +2,29 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const port = 4000
+const db = require('./db/db.js')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+  const { name, email, password, sub } = req.body
+  db.query(
+    'insert into users values (name, email, password, sub) values (?,?,?,?)',
+    [name, email, password, sub],
+    (error, results, fields) => {
+      if (error) {
+        console.error('Error al crear un nuevo usuario: ', error)
+        res.sendStatus(500)
+      } else {
+        res.json({ id: results.insertId })
+      }
+    }
+  )
 })
 
 app.listen(port, () => {
