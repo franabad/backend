a4600834d25bceab2fa8617e361a93175d832f30
Fran Abad <fam1096@hotmail.com>
2023-05-16 15:35:25 +0200

    Add new rows in database, send 500 and change email to name

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ app.get('/', (req, res) => {
 })
 
 app.post('/register', (req, res) => {
-  const { email, password } = req.body
+  const { email, password, name, lastname } = req.body
   bcrypt.hash(password, 10)
     .then(hashedPassword => {
-      db.query('insert into users (email, pass) values (?, ?)', [email, hashedPassword], (error, results) => {
+      db.query('insert into users (email, pass, name, lastname) values (?, ?, ?, ?)', [email, hashedPassword, name, lastname], (error, results) => {
         if (error) {
           console.error('Error al crear un nuevo usuario: ', error)
           res.status(500).json('El email proporcionado ya existe')
@@ -50,11 +50,11 @@ app.post('/login', (req, res) => {
       bcrypt.compare(password, user.pass)
         .then(result => {
           if (result) {
-            const sessionID = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET)
+            const sessionID = jwt.sign({ id: user.id, email: user.email, name: user.name }, process.env.JWT_SECRET)
             res.cookie('sessionID', sessionID, { maxAge: 120000, httpOnly: true })
             res.status(200).json({ id: user.id, email: user.email, sessionID })
           } else {
-            res.status(404).json('Usuario o contraseña incorrectos')
+            res.status(500).json(null)
           }
         })
     }
@@ -69,9 +69,9 @@ app.get('/login', (req, res) => {
 
   try {
     const decodedToken = jwt.verify(sessionID, process.env.JWT_SECRET)
-    const { email } = decodedToken
+    const { name } = decodedToken
     // aquí puede realizar cualquier verificación adicional que desee hacer antes de permitir que el usuario acceda a la página de inicio de sesión
-    res.status(200).json(`Bienvenido ${email}!`)
+    res.status(200).json(`Bienvenido, ${name}!`)
   } catch (error) {
     res.status(401).json('Sesión no válida')
   }
